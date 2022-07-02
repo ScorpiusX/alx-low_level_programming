a18cf4b3d2f367231d1d2dc87684b7cb946da5c6
Scorpius-X <eric.bolade@gmail.com>
2022-07-02 10:47:36 -0700

    ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 /**
+ * main - main block
  *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
+ * Return:Always 0(sucess)
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
