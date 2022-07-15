4f9f4af31a577655bd7a3b3ffd08a8e5dfe0319d
Scorpius-X <eric.bolade@gmail.com>
2022-07-14 19:01:45 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp -> compares
+ * @s1: param1
+ * @s2: param2
+ * Return: string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
