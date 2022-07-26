53f54e178df45fdc3756e8b1b17914662325bbf1
Scorpius-X <eric.bolade@gmail.com>
2022-07-26 07:11:15 -0700

    new commit

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * _strlen - return the length of the string
+ * _strlen - Return the length of the string
  * @s: char type pointer
+ *
  * Return: length of string
  */
 
 int _strlen(char *s)
 {
-	int c;
+	int len = 0;
 
-	for (c = 0; s[c] != '\0'; c++)
-		;
+	while (*s++)
+		len++;
 
-	return (c);
+	return (len);
 }
