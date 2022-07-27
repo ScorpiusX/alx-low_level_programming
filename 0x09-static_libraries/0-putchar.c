cea5e14e6b8bde26524a9f7abec5504ecb6fccfe
Scorpius-X <eric.bolade@gmail.com>
2022-07-27 15:20:34 +0100

    ok

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,20 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main - Print the word "_putchar"
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Always 0 (Success)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-int main(void)
+int _putchar(char c)
 {
-	int i;
-	char p[] = "_putchar";
-
-	for (i = 0; i < 8; i++)
-		{
-            _putchar(p[i]);
-		}
-		_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
