b2485506cec854737d061b720c29e293ca6c30a4
Scorpius-X <eric.bolade@gmail.com>
2022-07-27 14:57:20 +0100

    ok

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * _puts - print a string to stdout
- * @str: char array string type
- * Description: Can only use _putchar
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
  */
-
 void _puts(char *str)
 {
 	int i;
